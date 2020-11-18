05fceaefd495cf83437355e8545b4bc4f72854e3
Yibang Xiao <yibangxiao@gmail.com>
2020-11-18 02:15:47 +0000

    Changed repl.it file to add run command

--- linked_lists/main.cpp
+++ linked_lists/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "linked_lists.h"
 
-//using namespace std;
+using namespace std;
 
 int main () {
 
   MyLinkedLists mll;
   //cout << (mll.head)->val << endl;
-  std::cout << "Hi Yibang" << std::endl;
+  cout << "Hi Yibang" << endl;
 
   return 0;
 }
